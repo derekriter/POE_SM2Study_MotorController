df44f48848d6b081b0ca2f3eb1e6494090349dff
Derek Riter <derekriter08@gmail.com>
2026-03-15 18:25:52 -0400

    Tiny bit of work on control tab mode selection, broken

--- desktop_software/lib/widgets/control_tab.dart
+++ desktop_software/lib/widgets/control_tab.dart
@@ -1,10 +1,25 @@
 import 'package:desktop_software/app_state.dart';
+import 'package:desktop_software/device/device_control_mode.dart';
 import 'package:desktop_software/widgets/overflow_text.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 
-class ControlTab extends StatelessWidget {
+final _logger = Logger();
+
+class ControlTab extends StatefulWidget {
   const ControlTab({super.key});
+
+  @override
+  State<ControlTab> createState() => _ControlTabState();
+}
+
+class _ControlTabState extends State<ControlTab> {
+  static const _defaultMode = DeviceControlMode.stop;
+
+  final TextEditingController _modeController = TextEditingController();
+
+  var _selectedMode = _defaultMode;
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +29,31 @@ class ControlTab extends StatelessWidget {
       return Center(child: OverflowText("Please connect a device to control"));
     }
 
-    return Placeholder(child: Center(child: OverflowText("control")));
+    return Column(
+      children: [
+        DropdownMenu<DeviceControlMode>(
+          initialSelection: _defaultMode,
+          requestFocusOnTap: true,
+          enableSearch: false,
+          controller: _modeController,
+          onSelected: (selected) {
+            setState(() {
+              _selectedMode = selected ?? _defaultMode;
+            });
+            _logger.d(selected);
+          },
+          dropdownMenuEntries: [
+            const DropdownMenuEntry(
+              value: DeviceControlMode.stop,
+              label: "Stop",
+            ),
+            const DropdownMenuEntry(
+              value: DeviceControlMode.dutyCycle,
+              label: "Duty Cycle",
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
